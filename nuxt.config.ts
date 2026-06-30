ff6e03f443df08dd215978911c55f53ab9405893
Harlan Wilton <harlan@harlanzw.com>
2026-06-30 18:45:23 +1000

    chore: bump

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -429,6 +429,13 @@ export default defineNuxtConfig({
       templateParams: {
         separator: '·',
       },
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+      ],
       bodyAttrs: {
         class: 'antialiased font-sans text-neutral-700 dark:text-neutral-200 bg-white dark:bg-neutral-900',
       },
