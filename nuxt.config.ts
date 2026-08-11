fec873d900a93ddb27d6bbde2f9f1b1f73e516d1
Harlan Wilton <harlan@harlanzw.com>
2026-08-11 22:53:48 +1000

    chore: bump Nuxt DX to 0.0.6

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,7 +18,6 @@ export default defineNuxtConfig({
   nuxtDx: {
     report: true,
     sizeBudget: {
-      ignoreModules: ['@nuxt/icon', '@nuxt/ui', '@sentry/nuxt/module', 'motion-v'],
       overridesKb: { 'server/plugins/sentry.ts': 326 },
     },
   },
