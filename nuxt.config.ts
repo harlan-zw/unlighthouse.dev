3c41b2707dd8c28e2b6144dc3f7f4df74d80be8a
Harlan Wilton <harlan@harlanzw.com>
2026-08-11 21:06:14 +1000

    chore: configure accepted Nuxt budgets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,6 +17,10 @@ export default defineNuxtConfig({
 
   nuxtDx: {
     report: true,
+    sizeBudget: {
+      ignoreModules: ['@nuxt/icon', '@nuxt/ui', '@sentry/nuxt/module', 'motion-v'],
+      overridesKb: { 'server/plugins/sentry.ts': 326 },
+    },
   },
 
   modules: [
